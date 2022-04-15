1ddf4701e20fc8716c5f36590efc2956e9c9f23b
Geoffry Millner <geoff.millner22@gmail.com>
2022-04-15 09:25:23 -0700

    Makes base c# project to make phone book.

---
+++ Phone.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+class PhoneBook
+{
+  public static Dictionary<string, string> PhoneNumber = new Dictionary<string, string>();
+
+  static void Main()
+  {
+
+  }
+}
